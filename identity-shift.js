c9d728c53c8cb4b1b33e9dc972056008a827c1e7
Luiserebii <luis@serebii.io>
2019-06-12 21:41:07 -0400

    More refactoring, newIdentity and updateIdentity functions written out

--- identity-shift.js
+++ identity-shift.js
@@ -15,8 +15,15 @@ class IdentityShift {
 
 
   getIdentities(file = this.file) {
-    let identityStore = fs.existsSync(file) ? JSON.parse(fs.readFileSync(file)) : null);
+    let identityStore = fs.existsSync(file) ? JSON.parse(fs.readFileSync(file)) : {});
     return identityStore;
+  }
+
+  createIdentity(name, username, email, gpgKey = null) {
+    let identityStore = { name: {'username': username, 'email': email} };
+    if(gpgKey) identityStore[name].gpgKey = gpgKey;
+    return identityStore;
+
   }
 
   newIdentity(name, username, email, gpgKey = null, file = this.file) {
@@ -26,17 +33,29 @@ class IdentityShift {
     identityStore = this.getIdentities();
 
     //Add identity to store
-    identityStore.name = { 'username': username, 'email': email };
-    if(gpgKey) identityStore.name.gpgKey = gpgKey;
+    if(identityStore[name]){
 
-    //Finally, write identityStore to file
-    fs.writeFileSync(file, JSON.stringify(identityStore), 'utf8')
+      identityStore = Object.assign(identityStore, this.createIdentity(name, username, email, gpgKey));
+
+      //Finally, write identityStore to file
+      fs.writeFileSync(file, JSON.stringify(identityStore), 'utf8')
+    } else {
+      console.log("Identity already exists!")
+    }
 
   }
 
   updateIdentity(name, username, email, gpgKey = null, file = this.file) {
-    
+    let identityStore; 
 
+    //If file exists, let's load it in before writing
+    identityStore = this.getIdentities();
+
+    //Update identity
+    identityStore = Object.assign(identityStore, this.createIdentity(name, username, email, gpgKey));
+
+    //Finally, write identityStore to file
+    fs.writeFileSync(file, JSON.stringify(identityStore), 'utf8')
   }
 
 }
