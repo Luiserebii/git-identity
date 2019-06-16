8acef14b7d9f8763662cf99f20c1ffaa7c2cda89
Luiserebii <luis@serebii.io>
2019-06-15 20:20:50 -0400

    Updating for v0.2.0-alpha

--- meta.js
+++ meta.js
@@ -7,7 +7,7 @@
 
 const meta = {
 
-  version: '0.1.1',
+  version: '0.2.0-alpha',
   description: 'A command line tool allowing for easy shifting between git identities (username, email, and GPG).' 
 };
 
