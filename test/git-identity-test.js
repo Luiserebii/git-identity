8105ec66384a70eeb0d4a25a09765a7e6fe7b892
Luiserebii <luis@serebii.io>
2019-06-15 22:33:50 -0400

    Working on adding GitIdentity tests (some of them; some require native dedicated test build with git)

--- test/git-identity-test.js
+++ test/git-identity-test.js
@@ -4,21 +4,25 @@
 const chai = require('chai');
 const assert = chai.assert;
 
+const fs = require('fs');
+const path = require('path');
+
 const Identity = require('../src/git-identity/identity');
 
 
 describe('Class Identity', () => {
 
+  const id = {};
+
+  beforeEach(() => {
+    id.name = 'BTC';
+    id.username = 'Satoshi Nakamoto';
+    id.email = 'satoshin@gmx.com';
+    id.gpgKey = 'MQINBF0C';
+  })
+
+
   describe('toJSON() returns expected object', () => {
-
-    const id = {};
-
-    beforeEach(() => {
-      id.name = 'BTC';
-      id.username = 'Satoshi Nakamoto';
-      id.email = 'satoshin@gmx.com';
-      id.gpgKey = 'MQINBF0C';
-    })
 
     it('with name, username, email, GPG Key', () => {
       let identity = new Identity(id.name, id.username, id.email, id.gpgKey);
@@ -42,15 +46,6 @@ describe('Class Identity', () => {
 
   describe('toString() returns expected string', () => {
 
-    const id = {};
-
-    beforeEach(() => {
-      id.name = 'BTC';
-      id.username = 'Satoshi Nakamoto';
-      id.email = 'satoshin@gmx.com';
-      id.gpgKey = 'MQINBF0C';
-    })
-
     it('with name, username, email, GPG Key', () => {
       let identity = new Identity(id.name, id.username, id.email, id.gpgKey);
       let str = identity.toString();
@@ -72,5 +67,30 @@ describe('Class Identity', () => {
 
   })
 
+
+})
+
+describe('Class GitIdentity', () => {
+
+  let folder = '';
+  let file = '';
+ 
+  beforeEach(() => {
+    folder = path.resolve(__dirname, 'data-test');
+    file = path.resolve(folder, 'identities');
+    if(!fs.existsSync(folder)) { fs.mkdirSync(folder); }
+  })
+
+  it('listIdentities() returns expected object', () => {
+    
+    let identity = new Identity
+    
+    
+  })
+
+  //Clean up our test data folder
+  afterEach(() => {
+    if(fs.existsSync(folder)) { fs.rmdirSync(folder); }
+  })
 
 })
