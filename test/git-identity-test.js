56044c0aa9ef72347fdad2b0392e3db869e03dad
Luiserebii <luis@serebii.io>
2019-06-15 21:29:11 -0400

    Finished tests for Identity class

--- test/git-identity-test.js
+++ test/git-identity-test.js
@@ -29,6 +29,48 @@ describe('Class Identity', () => {
       assert.ok(json[id.name].gpgKey === id.gpgKey);
     })
 
+    it('with name, username, email, no GPG Key', () => {
+      let identity = new Identity(id.name, id.username, id.email);
+      let json = identity.toJSON();
+
+      assert.ok(json[id.name].username === id.username);
+      assert.ok(json[id.name].email === id.email);
+      assert.isNotOk(json[id.name].gpgKey);
+    })
+
   })
+
+  describe('toString() returns expected string', () => {
+
+    const id = {};
+
+    beforeEach(() => {
+      id.name = 'BTC';
+      id.username = 'Satoshi Nakamoto';
+      id.email = 'satoshin@gmx.com';
+      id.gpgKey = 'MQINBF0C';
+    })
+
+    it('with name, username, email, GPG Key', () => {
+      let identity = new Identity(id.name, id.username, id.email, id.gpgKey);
+      let str = identity.toString();
+
+      assert.ok(str.includes(id.name));
+      assert.ok(str.includes(id.email));
+      assert.ok(str.includes(id.gpgKey));
+    })
+
+    it('with username, email, no name or GPG Key', () => {
+      let identity = new Identity(id.username, id.email);
+      let str = identity.toString();
+
+      assert.isNotOk(str.includes(id.name) && str.includes('Name:'));
+      assert.ok(str.includes(id.email));
+      assert.isNotOk(str.includes(id.gpgKey) && str.includes('GPG Key:'));
+
+    })
+
+  })
+
 
 })
