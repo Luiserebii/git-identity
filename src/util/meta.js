dad337274762fa6ba13b8b588c4b0c79929aa341
Luiserebii <luis@serebii.io>
2019-06-18 21:22:04 -0400

    Finally fixed meta.js issue

--- src/util/meta.js
+++ src/util/meta.js
@@ -6,13 +6,13 @@
 'use strict';
 
 const fs = require('fs');
-
+const path = require('path');
 
 class Meta {
 
 
-  static readMetadata() {
-    const packageJSON = fs.readFileSync('./package.json');
+  static readMetadata(file=path.resolve(__dirname, '../', '../', 'package.json')) {
+    const packageJSON = fs.readFileSync(file);
     const metadata = JSON.parse(packageJSON); //No need to parse this manually, let's just give it as it is
 
     return metadata;
