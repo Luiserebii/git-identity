58ffd44ae3faf8297927293913e21d26018cc206
Luiserebii <luis@serebii.io>
2019-07-24 16:13:30 -0400

    Many typings added to git-identity.ts

--- src/git-identity/git-identity.ts
+++ src/git-identity/git-identity.ts
@@ -78,9 +78,9 @@ class GitIdentity {
   }
 
   //shift identity function implementation for application flag
-  shiftIdentity(name, file = this.file) {
-    let identityStore = this.getIdentities();
-    let id = identityStore[name];
+  shiftIdentity(name: string, file: string = this.file): boolean {
+    let identityStore: object = this.getIdentities();
+    let id: object = identityStore[name];
     if(id) {
       this.setIdentityGlobal(new Identity(name, id.username, id.email, id.gpgKey)); 
       return true;
@@ -90,9 +90,9 @@ class GitIdentity {
     }
   }
 
-  shiftIdentityLocal(name, file = this.file) {
-    let identityStore = this.getIdentities();
-    let id = identityStore[name];
+  shiftIdentityLocal(name: string, file: string = this.file): boolean {
+    let identityStore: object = this.getIdentities();
+    let id: object = identityStore[name];
     if(id) {
       this.setIdentityLocal(new Identity(name, id.username, id.email, id.gpgKey));
       return true;
@@ -103,39 +103,41 @@ class GitIdentity {
   }
 
 
-  getIdentityGlobal() {
-    let username = child_process.execSync('git config --global user.name', { encoding: 'utf8' }).trim();
-    let email = child_process.execSync('git config --global user.email', { encoding: 'utf8' }).trim();
-    let gpgKey = child_process.execSync('git config --global user.signingkey', { encoding: 'utf8' }).trim();
+  getIdentityGlobal(): object /*Identity*/ {
+    let username: string = child_process.execSync('git config --global user.name', { encoding: 'utf8' }).trim();
+    let email: string = child_process.execSync('git config --global user.email', { encoding: 'utf8' }).trim();
+    let gpgKey: string = child_process.execSync('git config --global user.signingkey', { encoding: 'utf8' }).trim();
     return new Identity(null, username, email, gpgKey);
   }
 
-  getIdentityLocal(){
-    let username = child_process.execSync('git config --local user.name', { encoding: 'utf8' }).trim();
-    let email = child_process.execSync('git config --local user.email', { encoding: 'utf8' }).trim();
-    let gpgKey = child_process.execSync('git config --local user.signingkey', { encoding: 'utf8' }).trim();
+  getIdentityLocal(): object /*Identity*/{
+    let username:string = child_process.execSync('git config --local user.name', { encoding: 'utf8' }).trim();
+    let email:string = child_process.execSync('git config --local user.email', { encoding: 'utf8' }).trim();
+    let gpgKey:string = child_process.execSync('git config --local user.signingkey', { encoding: 'utf8' }).trim();
     return new Identity(null, username, email, gpgKey);
   }
 
   //Set identity globally, run git commands to do so
-  setIdentityGlobal(identity) {
-    let cmd = `git config --global user.name "${identity.username}" && ` +
+  setIdentityGlobal(identity: object /*Identity*/): boolean {
+    let cmd: string = `git config --global user.name "${identity.username}" && ` +
                 `git config --global user.email "${identity.email}"`;
     if(identity.gpgKey) cmd += ` && git config --global user.signingkey "${identity.gpgKey}"`;
     child_process.execSync(cmd);
+    return true;
   }
 
   //Set identity locally
-  setIdentityLocal(identity) {
-    let cmd = `git config --local user.name "${identity.username}" && ` +
+  setIdentityLocal(identity: object /*Identity*/): boolean {
+    let cmd: string = `git config --local user.name "${identity.username}" && ` +
                 `git config --local user.email "${identity.email}"`;
     if(identity.gpgKey) cmd += ` && git config --local user.signingkey "${identity.gpgKey}"`;
     child_process.execSync(cmd);
+    return true;
   }
 
-  identitiesToString(identities) {
-    let str = '';
-    let keys = Object.keys(identities);
+  identitiesToString(identities: object /*Array of Identity objects*/): string {
+    let str: string = '';
+    let keys: object /*specifically, an array*/ = Object.keys(identities);
 
     str += new Identity(keys[0], identities[keys[0]].username, identities[keys[0]].email, identities[keys[0]].gpgKey).toString();
 
