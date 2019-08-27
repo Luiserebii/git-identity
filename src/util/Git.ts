e36eb1191fc0af4caf939fc1313f3e3be7dd4935
Luiserebii <luis@serebii.io>
2019-08-27 18:27:44 -0400

    Creating Git class

---
+++ src/util/Git.ts
@@ -0,0 +1,14 @@
+/**
+
+  Git class, this is meant to hold static functions which correspond to git internally, seperate out functionality relating to git vs GitIdentity class logic
+
+*/
+
+class Git {
+
+
+
+
+
+
+}
