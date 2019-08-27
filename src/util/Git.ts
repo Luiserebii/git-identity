002fcc17c8734797239917f08a5e63745d3623d5
Luiserebii <luis@serebii.io>
2019-08-27 18:40:42 -0400

    Adding getUserGlobal and generic exec() function

--- src/util/Git.ts
+++ src/util/Git.ts
@@ -4,9 +4,34 @@
 
 */
 
+import child_process = require('child_process');
+
+type GitUser {
+  name: string;
+  email: string;
+  signingKey?: string;
+
+}
+
 class Git {
 
+  /**
+   *  Executes a command, returns string (trimmed by default)
+   */
+  static exec(cmd: string, encoding = 'utf8', noTrim: = false): string {
+    let res: string = child_process.execSync(cmd, { 'encoding': encoding });
+    if(!noTrim) { res = res.trim(); }
 
+    return res;
+  }
+
+  static getUserGlobal(): GitUser {
+    let name: string = child_process.execSync('git config --global user.name', { encoding: 'utf8' }).trim();
+    let email: string = child_process.execSync('git config --global user.email', { encoding: 'utf8' }).trim();
+    let signingKey: string = child_process.execSync('git config --global user.signingkey', { encoding: 'utf8' }).trim();
+    const gitUser: GitUser = { 'name': name, 'email': email, 'signingKey': signingKey }; 
+    return gitUser;
+  }
 
 
 
