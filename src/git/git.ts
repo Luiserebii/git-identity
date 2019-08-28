3c9e5e7d220322d8e8ab34d6165ee20208bd352a
Luiserebii <luis@serebii.io>
2019-08-28 14:20:16 -0400

    Added code in event that we are stating both from email and from name (NOTE: there should a better way to contrcut this than what we have here)

--- src/git/git.ts
+++ src/git/git.ts
@@ -130,7 +130,22 @@ class Git {
             `export GIT_COMMITTER_NAME="$NEW_NAME"${newLine}`;
 
        //Close command
+       cmd += `fi${newLine}`;
 
+      cmd +=
+       `if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ] && [ "$GIT_AUTHOR_NAME" = "$OLD_NAME" ]
+        then${newLine}`;
+
+      if(opts.toEmail) {
+        cmd += 
+            `export GIT_AUTHOR_NAME="$NEW_EMAIL"${newLine}`;
+
+
+      if(opts.toName) {
+        cmd += 
+            `export GIT_AUTHOR_NAME="$NEW_NAME"${newLine}`;
+
+       //Close command
        cmd += `fi`;
     }
 
@@ -139,8 +154,17 @@ class Git {
          export GIT_AUTHOR_NAME="$NEW_NAME"
          export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
       fi
+
+    //Finally, close our command
+    cmd += `
       ' --tag-name-filter cat -- --branches --tags
     `;
+
+    //Execute
+    Util.exec(cmd);
+
+    return true;
+
 
   }
 
