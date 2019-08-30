1a44ef23f66e2fc9453da99b6a93b450df7282fb
Luiserebii <luis@serebii.io>
2019-08-30 03:21:40 -0400

    Light documentation of JSONMetadata

--- src/util/interfaces/json-metadata.ts
+++ src/util/interfaces/json-metadata.ts
@@ -1,3 +1,10 @@
+/**
+ * Interface JSONMetadata
+ * 
+ * @author Luiserebii <luis@serebii.io>
+ * @description Simple interface describing the expected properties we look for in a JSONMetadata object.
+ * TODO: Consider renaming this, why call it JSONMetadata? Why not something like ProjectMetadata?
+ */
 interface JSONMetadata {
   version: string;
   description: string;
