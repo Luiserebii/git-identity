5382cec33d3f170f4e48d3121d63e623bf02a5e3
Luiserebii <luis@serebii.io>
2019-08-30 03:17:53 -0400

    Documented meta.ts

--- src/util/meta.ts
+++ src/util/meta.ts
@@ -1,16 +1,24 @@
-/**
- * Meta class for metadata, or general configurable information on tool here pulled from package.json
- *
- */
 
 import fs = require('fs');
 import path = require('path');
 
 import JSONMetadata = require('./interfaces/json-metadata');
 
+/**
+ * Meta.ts
+ *
+ * @author Luiserebii <luis@serebii.io> 
+ * @description Meta class for metadata, or general configurable information on tool here pulled from package.json.
+ *
+ */
 class Meta {
 
-
+  /**
+   * Read metadata from a JSON file; this function in particular is intended to consume `package.json`.
+   * 
+   * @param {string} [file=path.resolve(__dirname, '../', '../', 'package.json')] - File to read metadata from.
+   * @returns {JSONMetadata} - An object compliant with the JSONMetadata interface.
+   */
   static readMetadata(file: string = path.resolve(__dirname, '../', '../', 'package.json')): JSONMetadata {
     const packageJSON: string = fs.readFileSync(file, 'utf8');
     const metadata: JSONMetadata = JSON.parse(packageJSON); //No need to parse this manually, let's just give it as is
